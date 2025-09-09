57e7349ff9211d0891a92916c9e2bdd673755f06
MonkeyNerdCoding <sockhi2004@gmail.com>
2025-09-10 00:40:42 +0700

    dbt model for analysis

---
+++ dbt/my_project/models/staging/stg_weather_data.sql
@@ -0,0 +1,25 @@
+{{config(
+    materialized='table',
+    unique_key='id'
+)}}
+
+with source as(
+    select * 
+    from {{ source('weather', 'raw_weather_data') }}
+),
+de_up as (
+    select *,
+        row_number() over(partition by id order by inserted_at desc) as rn
+    from source
+)
+
+select 
+    id,
+    city,
+    temperature,
+    weather_description,
+    wind_speed,
+    time as weather_time_local,
+    (inserted_at + (utc_offset || ' hours')::interval) as inserted_at_local
+from de_up
+where rn = 1
